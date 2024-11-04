6a9e1bd22713f9d07204bcf8f02425d135ebb7fa
Eugenio Guidi <eugenio.guidi@studio.unibo.it>
2024-11-04 11:14:14 +0100

    File IterableWithPolicyImpl.java with constructor added

---
+++ 71-custom-iterable/src/main/java/it/unibo/inner/impl/IterableWithPolicyImpl.java
@@ -0,0 +1,28 @@
+package it.unibo.inner.impl;
+
+import java.util.Iterator;
+
+import it.unibo.inner.api.IterableWithPolicy;
+import it.unibo.inner.api.Predicate;
+
+public class IterableWithPolicyImpl<T> implements IterableWithPolicy<T>{
+
+    private T[] array = null;
+
+    public IterableWithPolicyImpl(T[] array) {
+        this.array = array;
+    }
+
+    @Override
+    public Iterator iterator() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'iterator'");
+    }
+
+    @Override
+    public void setIterationPolicy(Predicate filter) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setIterationPolicy'");
+    }
+
+}
