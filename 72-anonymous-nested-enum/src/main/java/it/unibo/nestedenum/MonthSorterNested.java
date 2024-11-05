e382612b3c8df14bbe3efb06671676ad01db010e
Eugenio Guidi <eugenio.guidi@studio.unibo.it>
2024-11-05 23:06:02 +0100

    Implemented Enum month inside MonthSorterNested with months constant, field days and constructor

--- 72-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
+++ 72-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
@@ -9,6 +9,33 @@ import java.util.Objects;
  */
 public final class MonthSorterNested implements MonthSorter {
 
+    private static final int TIMES = 100;
+    private static final int SHORT_MONTH = 28;
+    private static final int USUAL_MONTH = 30;
+    private static final int LONG_MONTH = 31;
+
+    public enum Month {
+
+        JANUARY(LONG_MONTH), 
+        FEBRUARY(SHORT_MONTH), 
+        MARCH(LONG_MONTH), 
+        APRIL(USUAL_MONTH), 
+        MAY(LONG_MONTH), 
+        JUNE(USUAL_MONTH), 
+        JULY(LONG_MONTH), 
+        AUGUST(LONG_MONTH), 
+        SEPTEMBER(USUAL_MONTH), 
+        OCTOBER(LONG_MONTH), 
+        NOVEMBER(USUAL_MONTH), 
+        DECEMBER(LONG_MONTH);
+
+        private int days;
+
+        Month(final int days) {
+            this.days = days;
+        }
+    }
+
     @Override
     public Comparator<String> sortByDays() {
         return null;
