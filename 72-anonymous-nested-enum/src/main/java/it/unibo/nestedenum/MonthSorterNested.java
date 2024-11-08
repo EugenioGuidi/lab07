9e7aa806434cf22e3d762bdfff04d150470f5906
Eugenio Guidi <eugenio.guidi@studio.unibo.it>
2024-11-08 19:34:33 +0100

    Added enum Month whit constructor

--- 72-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
+++ 72-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
@@ -9,6 +9,10 @@ import java.util.Objects;
  */
 public final class MonthSorterNested implements MonthSorter {
 
+    private static final int SHORT_MONTH = 28;
+    private static final int USUAL_MONTH = 30;
+    private static final int LONG_MONTH = 31;
+
     @Override
     public Comparator<String> sortByDays() {
         return null;
@@ -18,4 +22,26 @@ public final class MonthSorterNested implements MonthSorter {
     public Comparator<String> sortByOrder() {
         return null;
     }
+
+    public enum Month {
+        JANUARY("january", LONG_MONTH),
+        FEBRUARY("february", SHORT_MONTH),
+        MARCH("march", LONG_MONTH),
+        APRIL("april", USUAL_MONTH),
+        MAY("may", LONG_MONTH),
+        JUNE("june", USUAL_MONTH),
+        JULY("july", LONG_MONTH),
+        AUGUST("august", LONG_MONTH),
+        SEPTEMBER("september", USUAL_MONTH),
+        OCTOBER("october", LONG_MONTH),
+        NOVEMBER("november", USUAL_MONTH),
+        DECEMBER("december", LONG_MONTH);
+
+        private String monthName;
+        private int days;
+
+        private Month(final String monthName, final int days) {
+            this.monthName = monthName;
+            this.days = days;
+        }
 }
