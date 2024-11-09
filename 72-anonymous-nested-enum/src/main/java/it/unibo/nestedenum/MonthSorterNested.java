fe5715a23642f34ca984cbf753c5df1c0c0559bc
Eugenio Guidi <eugenio.guidi@studio.unibo.it>
2024-11-09 14:47:22 +0100

    Implemented methods SortByDate and SortByMonthOrder called by SortByDays and SortByOrder and implemented method fromString in enum class

--- 72-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
+++ 72-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
@@ -15,53 +15,57 @@ public final class MonthSorterNested implements MonthSorter {
 
     @Override
     public Comparator<String> sortByDays() {
-        return new Comparator<String>() {
-
-            @Override
-            public int compare(String o1, String o2) {
-                int daysO1 = getMonth(o1).getDays();
-                int daysO2 = getMonth(o2).getDays();
-                if(daysO1 < daysO2) {
-                    return -1;
-                }
-                if(daysO1 == daysO2) {
-                    return 0;
-                }
-                if(daysO1 > daysO2) {
-                    return 1;
-                }
-                throw new IllegalArgumentException("Days o1 or o2 not valid in compartorByDays");
-            }
-            
-        };
+        return new SortByDate();
     }
 
     @Override
     public Comparator<String> sortByOrder() {
-        return new Comparator<String>() {
-
-            @Override
-            public int compare(String o1, String o2) {
-                int daysO1 = getMonth(o1).ordinal();
-                int daysO2 = getMonth(o2).ordinal();
-                if(daysO1 < daysO2) {
-                    return -1;
-                }
-                if(daysO1 == daysO2) {
-                    return 0;
-                }
-                if(daysO1 > daysO2) {
-                    return 1;
-                }
-                throw new IllegalArgumentException("Days o1 or o2 not valid in compartorByOrder");
-            }
-              
-        };
+        return new SortByMonthOrder();
     }
 
-    private Month getMonth(String nameMonth) {
-        //qui devo richiamare probabilmente fromToString
-        return Month.valueOf(nameMonth.toUpperCase());
+    public static class SortByDate implements Comparator<String> {
+
+        @Override
+        public int compare(String o1, String o2) {
+            int daysO1 = getMonth(o1).getDays();
+            int daysO2 = getMonth(o2).getDays();
+            if(daysO1 < daysO2) {
+                return -1;
+            }
+            if(daysO1 == daysO2) {
+               return 0;
+            }
+            if(daysO1 > daysO2) {
+                return 1;
+            }
+            throw new IllegalArgumentException("Days o1 or o2 not valid in compartorByDays");
+        }
+        
+    }
+
+    public static class SortByMonthOrder implements Comparator<String> {
+
+        @Override
+        public int compare(String o1, String o2) {
+            int daysO1 = getMonth(o1).ordinal();
+            int daysO2 = getMonth(o2).ordinal();
+            if(daysO1 < daysO2) {
+                return -1;
+            }
+            if(daysO1 == daysO2) {
+                return 0;
+            }
+            if(daysO1 > daysO2) {
+                return 1;
+            }
+            throw new IllegalArgumentException("Days o1 or o2 not valid in compartorByOrder");
+        }
+        
+    }
+
+    private static Month getMonth(String nameMonth) {
+
+        return Month.fromString(nameMonth);
     }
 
     public enum Month {
@@ -78,19 +82,36 @@ public final class MonthSorterNested implements MonthSorter {
         NOVEMBER("november", USUAL_MONTH),
         DECEMBER("december", LONG_MONTH);
 
-        private String monthName;
+        private String nameMonth;
         private int days;
 
         private int getDays() {
             return this.days;
         }
 
+        private String getNameMonth() {
+            return this.nameMonth;
+        }
+
         private Month(final String monthName, final int days) {
-            this.monthName = monthName;
+            this.nameMonth = monthName;
             this.days = days;
         }
+
+        private static Month fromString(String nameMonth) {
+            Month matchedMonth = null;
+            for(Month month : Month.values()) {
+                if(month.getNameMonth().startsWith(nameMonth.toLowerCase())) {
+                    if(matchedMonth != null) {
+                        throw new IllegalStateException("String that match more then one month");
+                    }
+                    matchedMonth = month;
+                }
+            }
+            if(matchedMonth == null) {
+                throw new IllegalStateException("String that don't found a month");
+            }
+            return matchedMonth;
+        }
     }
-
-
-
 }
